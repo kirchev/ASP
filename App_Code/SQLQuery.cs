284812e6d11d1cf89879159a0429fbe5e5c2ec3f
kirchev <2203@unibit.bg>
2017-03-13 14:33:15 +0200

    asp 2 част

---
+++ App_Code/SQLQuery.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for SQLQuery
+/// </summary>
+public class SQLQuery
+{
+    public const string NewsInserts = "Insert into news (Title, Author, Content, CreateDate, Image) VALUES(@Title, @Author, @Content, @CreateDate, @Image)";
+    public const string InsertActivationCode = "INSERT INTO UserActivationCode VALUES(@UserId, @ActivationCode)";
+}
