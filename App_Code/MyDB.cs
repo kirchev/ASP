fe57ce0a866d6278af49738935b2d72c1a23bae8
kirchev <2203@unibit.bg>
2017-03-27 12:23:04 +0300

    global

---
+++ App_Code/MyDB.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for MyDB
+/// </summary>
+public class MyDB
+{
+    public static void Insert(string title, string author, string content, System.Web.UI.WebControls.FileUpload FU_Image)
+    {
+        Guid guid = Guid.NewGuid();
+        string url = "";
+        string ConnectionString = ConfigurationManager.ConnectionStrings["ConnString"].ConnectionString;
+        SqlConnection conn = new SqlConnection(ConnectionString);
+
+        try
+        {
+            if (FU_Image.PostedFile != null)
+            {
+                string fileExtention = Path.GetExtension(FU_Image.FileName.ToLower());
+                if (fileExtention == ".jpeg" || fileExtention == ".jpg" || fileExtention == ".png")
+                {
+                    string FileName = Path.GetFileName(FU_Image.PostedFile.FileName);
+                    string rename = guid + FileName.Replace("", "_");
+                    FU_Image.SaveAs(System.Web.HttpContext.Current.Server.MapPath("~pages/news/newsimages/" + rename));
+
+                    url = "~pages/news/newsimages/" + rename;
+                }
+            }
+            conn.Open();
+            using (SqlCommand cmd = new SqlCommand())
+            {
+                cmd.Connection = conn;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = SQLQuery.NewsInserts;
+                cmd.Parameters.AddWithValue("@Title",title);
+                cmd.Parameters.AddWithValue("@Author",author);
+                cmd.Parameters.AddWithValue("@Content",content);
+                cmd.Parameters.AddWithValue("@CreateDate",String.Format("{0:d/M/yyyy HH:mm:ss}", DateTime.Now));
+                cmd.Parameters.AddWithValue("@Image",url);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        catch (Exception e)
+        {
+
+        }
+
+    }
+}
